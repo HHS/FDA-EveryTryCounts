370d5dfc99a810462fcf36d31732641d9060f1e6
ameza <ameza@rescueagency.com>
2020-03-05 13:29:02 -0800

    removed unnessesary comments

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php 
-    // I have realized that the past and future are real illusions, 
-    // that they exist in the present, which is what there is and all there is.
     require(__DIR__ . "/app/core.php"); 
 ?>
