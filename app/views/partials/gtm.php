6ebf01765261d6de24b1b4bbd74065bbbfaa1c3c
Ally <aautotte@rescueagency.com>
2020-03-13 11:27:45 -0700

    updated GA & GTM

--- app/views/partials/gtm.php
+++ app/views/partials/gtm.php
@@ -1,20 +1,28 @@
 <!-- Google Tag Manager -->
-<script>
-	if (location.hostname == 'smokefree.gov' || location.hostname == 'www.smokefree.gov') {
-		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push(
-			{'gtm.start': new Date().getTime(),event:'gtm.js'}
-		);var f=d.getElementsByTagName(s)[0],
-			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','');
-	}
-	else {
-		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push(
-			{'gtm.start': new Date().getTime(),event:'gtm.js'}
-		);var f=d.getElementsByTagName(s)[0],
-			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','');
-	}
-</script>
+<!--<script>-->
+<!--	if (location.hostname == 'smokefree.gov' || location.hostname == 'www.smokefree.gov') {-->
+<!--		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push(-->
+<!--			{'gtm.start': new Date().getTime(),event:'gtm.js'}-->
+<!--		);var f=d.getElementsByTagName(s)[0],-->
+<!--			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=-->
+<!--			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);-->
+<!--		})(window,document,'script','dataLayer','');-->
+<!--	}-->
+<!--	else {-->
+<!--		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push(-->
+<!--			{'gtm.start': new Date().getTime(),event:'gtm.js'}-->
+<!--		);var f=d.getElementsByTagName(s)[0],-->
+<!--			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=-->
+<!--			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);-->
+<!--		})(window,document,'script','dataLayer','');-->
+<!--	}-->
+<!--</script>-->
+<!-- End Google Tag Manager -->
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NK84NDQ');</script>
 <!-- End Google Tag Manager -->
