ca381e820449cf281f311a61e963509c9f6bcca1
Ally <aautotte@rescueagency.com>
2020-03-27 11:32:27 -0700

    gtm updates

--- app/assets/js/vendor/floodlight.js
+++ app/assets/js/vendor/floodlight.js
@@ -1,6 +1,6 @@
-// function makeFloodlightTagCall(value) {
-//     gtag('event', 'conversion', {
-//         'allow_custom_scripts': true,
-//         'send_to': value
-//     });
-// }
+function makeFloodlightTagCall(value) {
+    gtag('event', 'conversion', {
+        'allow_custom_scripts': true,
+        'send_to': value
+    });
+}
